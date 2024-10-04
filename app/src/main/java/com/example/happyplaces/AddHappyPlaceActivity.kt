ed3051e12fa3f23bf461beb777ed8082c12aa4b9
octopus-idil <oncuidil@gmail.com>
2024-10-04 18:02:09 +0300

    saved photos and permissions

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,87 @@
+package com.example.happyplaces
+
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
+    private var binding: ActivityAddHappyPlaceBinding? = null
+    private var calender = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        setSupportActionBar(binding!!.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding!!.toolbarAddPlace.setNavigationOnClickListener{
+            onBackPressed()
+        }
+        dateSetListener=DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calender.set(Calendar.YEAR,year)
+            calender.set(Calendar.MONTH,month)
+            calender.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateInView()
+        }
+        binding!!.etDate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date->{
+                DatePickerDialog(this@AddHappyPlaceActivity,dateSetListener,
+                    calender.get(Calendar.YEAR),
+                    calender.get(Calendar.MONTH),
+                    calender.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        }
+    }
+    private fun updateInView(){
+        val myFormat="dd.MM.yyyy"
+        val sdf= SimpleDateFormat(myFormat, Locale.getDefault())
+        binding!!.etDate.setText(sdf.format(calender.time).toString())
+    }
+
+    private fun takePhotoFromCamera(){
+        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
+}
---
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.happyplaces
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.happyplaces.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
